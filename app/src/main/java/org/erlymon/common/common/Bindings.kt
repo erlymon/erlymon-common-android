8fa3cda78f6b91c7338fd289d998b0a12167874c
Sergey Penkovsky <dev@erlymon.org>
2018-07-30 09:05:02 +0300

    cleaned code

--- app/src/main/java/org/erlymon/common/common/Bindings.kt
+++ app/src/main/java/org/erlymon/common/common/Bindings.kt
@@ -1,14 +1,11 @@
 package org.erlymon.common.common
 
-import android.view.View
-import android.widget.Toast
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import org.erlymon.common.adapter.CheckDelegateAdapter
 import org.erlymon.common.adapter.CompositeDelegateAdapter
 import org.erlymon.common.adapter.OnItemListener
 import org.erlymon.common.adapter.TextDelegateAdapter
-import org.erlymon.common.model.TextBean
 
 object Bindings {
     @BindingAdapter(value = ["items", "onSelectItem"], requireAll = false)
@@ -16,15 +13,6 @@ object Bindings {
         items?.apply {
             if (adapter == null) {
                 adapter = CompositeDelegateAdapter.Builder<Any>()
-                        /*.add(TextDelegateAdapter(object: OnItemListener {
-                            override fun onClick(view: View, item: Any) {
-                                when (item) {
-                                    is TextBean -> {
-                                        Toast.makeText(view.context, "TEXT BEAN: $item", Toast.LENGTH_LONG).show()
-                                    }
-                                }
-                            }
-                        }))*/
                         .add(TextDelegateAdapter(listener))
                         .add(CheckDelegateAdapter())
                         .build()
